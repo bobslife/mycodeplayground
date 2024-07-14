ace5b49ae7d533bd8625273f9f5f3d923bdf7812
Bob-Park (Chanmin Park) <chanmin9401@gmail.com>
2024-07-14 22:43:04 +0900

    commit test2

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          commit test
+          commit test2
         </a>
       </header>
     </div>
